f029ca712bf7e1d23f98c558bbde02f9bdce4271
David Beaumont <david.beaumont+github@gmail.com>
2024-12-31 02:25:22 +0000

    Merge pull request #7
    * Trying out dynamic interfaces.

--- src/day4.rs
+++ src/day4.rs
@@ -1,40 +1,40 @@
 use itertools::Itertools;
 use strum::IntoEnumIterator;
-use crate::grid::{parse_lines, Grid, GridLike};
-use crate::point::{Dir, Point, GridIter, PointLike};
-use crate::point::Dir::{LeftDown, LeftUp, RightDown, RightUp};
+use crate::grid::{parse_lines, Grid, GridIter};
+use crate::point::{Dir, Point, PointLike};
 
 pub fn run(input: &str) -> (usize, usize) {
     let grid = parse_lines(input);
     let part1 = grid.indexed_iter()
-        .filter(|(_, &c)| c == 'X')
-        .map(|(p, _)| count_xmas(&grid, &Point::of(&p)))
+        .filter(|&(_, c)| c == 'X')
+        .map(|(p, _)| count_xmas(&grid, &p))
         .sum();
     let part2 = grid.indexed_iter()
-        .filter(|(p, &c)| c == 'A' && is_x_mas(&grid, &Point::of(p)))
+        .filter(|&(_, c)| c == 'A')
+        .filter(|&(p, _)| is_x_mas(&grid, &p))
         .count();
     (part1, part2)
 }
 
-fn count_xmas(grid: &Grid, p: &Point) -> usize {
+fn count_xmas(grid: &Grid, x_pos: &Point) -> usize {
+    // All directions (including diagonals).
     Dir::iter()
-        .filter(|&d| is_mas(
-            // Step 1 forward to read after 'X'.
-            GridIter::new(p.step(d, 1), d).map_while(|p| grid.at(&p)).next_tuple()))
+        // Step forward 1 to read after 'X'.
+        .filter(|&d| is_mas(&mut grid.walk(x_pos.step(d, 1), d)))
         .count()
 }
 
-fn is_x_mas(grid: &Grid, p: &Point) -> bool {
+fn is_x_mas(grid: &Grid, a_pos: &Point) -> bool {
     // grid[p] == 'A', an X is formed if 2 diagonals read "M A S".
-    [RightUp, LeftUp, LeftDown, RightDown].into_iter()
-        .filter(|&d| is_mas(
-            // Step 1 backward to read before 'A'.
-            GridIter::new(p.step(d, -1), d).map_while(|p| grid.at(&p)).next_tuple()))
+    Dir::diagonals()
+        // Step backward 1 to read before 'A'.
+        .filter(|&d| is_mas(&mut grid.walk(a_pos.step(d, -1), d)))
         .count() == 2
 }
 
-fn is_mas(p: Option<(char, char, char)>) -> bool {
-    match p {
+// Alternatively, call next_tuple() on the callers and accept Option<(char, char, char)> here.
+fn is_mas(it: &mut GridIter) -> bool {
+    match it.next_tuple() {
         Some((m, a, s)) if m == 'M' && a == 'A' && s == 'S' => true,
         _ => false
     }
